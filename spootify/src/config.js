e5292328dc0685c35516d1d67fa7c7f9d0b05ab4
kim <82406962+KristinaKim6686@users.noreply.github.com>
2022-04-30 15:32:34 +0300

    got the token and the first fetch

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -1,8 +1,8 @@
 export default {
   api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
     clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
-  }
-}
+    clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+  },
+};
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import axios from "axios";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import { api, clientId, clientSecret } from "../../../config";
 
 export default class Discover extends Component {
   constructor() {
@@ -9,8 +11,40 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
+      token: "",
     };
+  }
+
+  componentDidMount() {
+    const authUrl = "https://accounts.spotify.com/api/token";
+    const clientId = "d33f5f01889247fc9c955c3645259684";
+    const clientSecret = "0cc7cd9eb02940499d6d9f959adc0f4d";
+    const baseUrl = "https://api.spotify.com/v1";
+
+    // setting the access token
+    axios(authUrl, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+      },
+      data: "grant_type=client_credentials",
+      method: "POST",
+    }).then((tokenResponse) => {
+      this.setState({ token: tokenResponse.data.access_token });
+
+      //  new releases fetch when the page is loading
+
+      axios("https://api.spotify.com/v1/browse/new-releases", {
+        headers: {
+          Authorization: "Bearer " + tokenResponse.data.access_token,
+        },
+        method: "GET",
+      }).then((response) => {
+        this.setState({ newReleases: response.data.albums.items });
+        console.log(response);
+      });
+    });
   }
 
   render() {
@@ -18,9 +52,22 @@ export default class Discover extends Component {
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }
